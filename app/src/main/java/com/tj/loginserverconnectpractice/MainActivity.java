acb713ee399a91b66525c5892f595156020eeffb
cunjj86 <ljgcunjj@naver.com>
2019-05-12 16:20:35 +0900

    로그인 서버 연습 최종 버전

--- app/src/main/java/com/tj/loginserverconnectpractice/MainActivity.java
+++ app/src/main/java/com/tj/loginserverconnectpractice/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bumptech.glide.Glide;
 import com.tj.loginserverconnectpractice.databinding.ActivityMainBinding;
 import com.tj.loginserverconnectpractice.utils.ConnectServer;
 
@@ -55,7 +56,43 @@ public class MainActivity extends BaseActivity {
 
                                 JSONObject data = json.getJSONObject("data");
 
+                                JSONObject user = data.getJSONObject("user");
 
+//                                프사 경로
+                                String profile_image = user.getString("profile_image");
+//                                사용자 이름
+                                String name = user.getString("name");
+//                                보유 잔고
+                                int balance = user.getInt("balance");
+
+//                                은행 로고를 따기 전에 bank_code JSONObject 먼저
+                                JSONObject bank_code = user.getJSONObject("bank_code");
+
+//                                은행 정보 안에 있는 로고 경로
+                                String logo = bank_code.getString("logo");
+//                                은행 이름
+                                String bankName = bank_code.getString("name");
+
+//                                사용자 계좌 번호
+                                String billing_account = user.getString("billing_account");
+
+//                                프로필 이미지 출력
+                                Glide.with(mContext).load(profile_image).into(act.profileImg);
+
+//                                사용자 이름 출력
+                                act.userNameTxt.setText(name);
+
+//                                사용자 보유 잔고를 , 찍는 양식으로
+                                act.userBalnceTxt.setText(String.format("%,d P", balance));
+
+//                                은행 로고 출력
+                                Glide.with(mContext).load(logo).into(act.bankLogoImg);
+
+//                                은행 이름 출력
+                                act.bankNameTxt.setText(bankName);
+
+//                                사용자 은행 계좌 출력
+                                act.bankAccountTxt.setText(billing_account);
 
                             }
                             else {
