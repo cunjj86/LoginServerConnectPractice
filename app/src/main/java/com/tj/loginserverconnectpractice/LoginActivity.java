b158d9bd31333ace768751d449112a656f7c4e3c
Kyungjin Cho <kj_cho@idealidea.co.kr>
2019-05-12 12:50:35 +0900

    로그인 결과 로그캣에 출력하기

--- app/src/main/java/com/tj/loginserverconnectpractice/LoginActivity.java
+++ app/src/main/java/com/tj/loginserverconnectpractice/LoginActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.tj.loginserverconnectpractice.databinding.ActivityLoginBinding;
+import com.tj.loginserverconnectpractice.utils.ConnectServer;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity {
 
@@ -26,10 +29,23 @@ public class LoginActivity extends BaseActivity {
             public void onClick(View v) {
 
 //                1. 아이디와 비번을 뭐라고 입력했는지? 받아오기
+
+                String inputId = act.loginIdEdt.getText().toString();
+                String inputPw = act.loginPwEdt.getText().toString();
+
 //                2. 받아온 아이디와 비번이 정말로 올바른 회원지? 검사
 //                 아이디/비번이 모두 동일한 사람이 회원 명부에 있는지? 검사.
 
-//                test123 / test123
+                ConnectServer.postRequestSignIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+
+
+                    }
+                });
+
+//                test123 / Test!123
 //                iu0001 / Test!123
 //                testorder1 / testorder1
 //                gggg1111 / gggg1111
